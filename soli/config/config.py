116dac3c5d3f098d2289b4c64bd09e42cc251e37
Bob Rank <rankrh@gmail.com>
2019-09-07 09:47:02 -0600

    config

---
+++ soli/config/config.py
@@ -0,0 +1,19 @@
+from configparser import ConfigParser
+import os
+
+DIRNAME = os.path.dirname(__file__)
+FILENAME = "/soliConfig.ini"
+def config(filename=DIRNAME + FILENAME, section='soliConfig'):
+    parser = ConfigParser()
+    parser.read(filename)
+    db = {}
+
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+
+    else:
+        raise Exception(f'Section {section} not found in {filename}')
+
+    return db
--- soli/settings.py
+++ soli/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-from config import config
+from .config import config
 from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
