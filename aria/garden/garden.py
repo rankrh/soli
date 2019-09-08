113a556a22e8001aa681bbaa0e311aca70db0e78
Bob Rank <rankrh@gmail.com>
2019-09-07 21:55:09 -0600

    Add garden class

---
+++ aria/garden/garden.py
@@ -0,0 +1,14 @@
+from plot import Plot
+
+class Garden:
+    def __init__(
+        self,
+        plots,
+        spacing,
+        length,
+        width
+    ):
+        this.plots = plots
+        this.spacing = spacing
+        this.length = length
+        this.width = width
