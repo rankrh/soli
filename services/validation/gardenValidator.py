48785a52251a2697f1fe392a993ab540917e686a
Bob Rank <rankrh@gmail.com>
2019-11-23 21:27:06 -0700

    Delete old files

--- services/validation/gardenValidator.py
+++
@@ -1,5 +0,0 @@
-from .geometryValidator import validateGeometry
-
-def validateGarden():
-
-    validateGeometry(shape, center, radius, coordinates)
--- services/validation/plotValidator.py
+++
@@ -1,4 +0,0 @@
-from .geometryValidator import validateGeometry
-
-def validatePlot(crop, shape, center, radius, coordinates):
-    validateGeometry(shape, center, radius, coordinates)
