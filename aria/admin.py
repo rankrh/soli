8dbe1f363c4694ecdabc405365dc3654e0c5edd8
Bob Rank <rankrh@gmail.com>
2020-10-17 21:51:02 -0600

    Move up directory

--- aria/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Crop, Genus, Species, Subspecies
-
-admin.site.register(Crop)
-admin.site.register(Species)
-admin.site.register(Genus)
-admin.site.register(Subspecies)
--- aria/views/__init__.py
+++
@@ -1,4 +0,0 @@
-from .index import index
-from aria.views.create.crop import createCrop
-from aria.views.create.species import createSpecies, createSpeciesAjax
-from aria.views.display.crops import displayCrops
