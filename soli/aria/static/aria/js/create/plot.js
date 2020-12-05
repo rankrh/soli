f59490af201c7beeb4199cb49d3a6647dcca3ac7
Bob Rank <rankrh@gmail.com>
2020-12-04 17:58:47 -0700

    Playing around. Might want to stick with mapbox for now

---
+++ soli/aria/static/aria/js/create/plot.js
@@ -0,0 +1,9 @@
+var map = L.map('mapid').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={pk.eyJ1IjoicmFua3JoIiwiYSI6ImNraDFnbjlrcTAxZjMydG4xN2dyNmtoYWUifQ.tlJBm2GyxVZapHdK0_oDyQ}', {
+	maxZoom: 18,
+	id: 'mapbox/streets-v11',
+	tileSize: 512,
+	zoomOffset: -1,
+	accessToken: 'pk.eyJ1IjoicmFua3JoIiwiYSI6ImNraDFnbjlrcTAxZjMydG4xN2dyNmtoYWUifQ.tlJBm2GyxVZapHdK0_oDyQ'
+}).addTo(map);
