56ec88af5da6aa0e31ac42a60661209199f013ce
Bob <rankrh@gmail.com>
2020-12-10 15:37:08 -0700

    Saving progress

--- soli/aria/static/aria/js/create/plot.js
+++ soli/aria/static/aria/js/create/plot.js
@@ -62,6 +62,12 @@ function initializePlots() {
 	updateAllPolygonAreas();
 }
 
+function expandPlotDetails(plotId) {
+
+	togglePlotFocus(plotId);
+	$("#plot-" + plotId + "-tools").toggle();
+}
+
 function togglePlotFocus(plotId) {
 
 	var boundedLayer = featureGroup;
@@ -82,7 +88,7 @@ function zoomToBounds(boundedLayer) {
 
 function editPlotDetails(plot) {
 
-	$("#edit-plot-id").val(plot.id);
+	$("#edit-plot-id").val(plot._leaflet_id);
 
 	if (plot.name) {
 		$("#edit-plot-name").val(plot.name)
@@ -135,6 +141,9 @@ function persistPlot(plot) {
 	});
 }
 
+function deletePlot(plot) {
+	deletePlots([plot]);
+}
 
 function deletePlots(plots) {
 
@@ -147,7 +156,9 @@ function deletePlots(plots) {
 			plots: JSON.stringify(plots)
 		},
 		success: function(data) {
-			console.log("success");
+			for (var i = 0; i < plots.length; i++) {
+				deletePlotAccordion(plots[i]);
+			}
 		}
 	});
 }
@@ -159,30 +170,40 @@ function createPlotAccordion(plot) {
 	var name = plot.name ? plot.name : "Unnamed Plot"
 	var description = plot.description ? plot.description : ""
 
-	var newAccordion =  '<div id="plot-details-' + num + '" class="card" onclick="togglePlotFocus(' + num + ')">'
-	newAccordion += '	<div class="card-header" id="heading-' + num + '">'
-	newAccordion += '		<h2 class="mb-0">'
-	newAccordion += '			<button id="plot-' + num + '-name" class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapse-plot-' + num + '" aria-expanded="true" aria-controls="collapse-plot-' + num + '">' + name + '</button>'
-    newAccordion += '		</h2>'
-    newAccordion += '	</div>'
-    newAccordion += '	<div id="collapse-plot-' + num + '" class="collapse" aria-labelledby="heading-plot-' + num + '" data-parent="#plots-accordion">'
-    newAccordion += '	<div class="card-body">'
-    newAccordion += '		<div class="card-body">'
-    newAccordion += '			<div id="plot-' + num + ' description">'
-    newAccordion += '				<p>' + description + '</p>'
-	newAccordion += '			</div>'
+	var newAccordion = '<div id="plot-details-' + num + '" class="list-group-item" onclick="togglePlotFocus(' + num + ')">'
+	newAccordion += '	<h3><div id="plot-' + num + '-name" class="plot-header text-left" id="heading-' + num + '" data-toggle="collapse" data-target="#collapse-plot-' + num + '" aria-expanded="true" aria-controls="collapse-plot-' + num + '">'
+	newAccordion += name
+	newAccordion += '	</div></h3>'
+	newAccordion += '	<div id="collapse-plot-' + num + '" class="collapse" aria-labelledby="heading-plot-' + num + '" data-parent="#plots-accordion">'
+	newAccordion += '		<div class="card-body">'
+	newAccordion += '			<button class="btn btn-sm btn-light justify-content-center align-self-center" onclick="editPlotDetails(featureGroup.getLayer(' + num + '))">'
+	newAccordion += '				<svg viewBox="0 0 8 8" class="icon">'
+	newAccordion += '					<use xlink:href="#pencil"></use>'
+	newAccordion += '				</svg>'
+	newAccordion += '			</button>'
+	newAccordion += '		<div id="plot-' + num + '-description">'
+	newAccordion += '			<p>' + description + '</p>'
 	newAccordion += '		</div>'
 	newAccordion += '	</div>'
 	newAccordion += '</div>'
+
 
 	$("#plots-accordion").append(newAccordion);
 }
 
 function updatePlotAccordion(plot) {
 
-	$("#plot-" + plot._leaflet_id + "-name").val(plot.name);
-	$("#plot-" + plot._leaflet_id + "-description").val(plot.description);
-	$("#plot-" + plot._leaflet_id + "-description").val(plot.description);
+	$("#plot-" + plot._leaflet_id + "-name").text(plot.name);
+	$("#plot-" + plot._leaflet_id + "-description").text(plot.description);
+}
+
+function deletePlotAccordion(plotId) {
+
+	$("#plot-details-" + plotId).remove();
+}
+
+function verifyDelete(plot) {
+
 }
 
 function clearEditPlotModal() {
@@ -204,7 +225,6 @@ function addSavedPlots() {
 		plotLayer.addTo(map);
 
 	  	featureGroup.addLayer(plotLayer);
-		updatePolygonPopup(plotLayer, units);
 	}
 }
 
