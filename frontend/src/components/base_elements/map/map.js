b7be01287ed62db4012d971c76c744a9be1289b7
Bob Rank <brank@goodwaygroup.com>
2021-05-31 23:28:48 -0600

    Leaflet with mapbox map

--- frontend/src/components/base_elements/map/map.js
+++ frontend/src/components/base_elements/map/map.js
@@ -1,51 +1,40 @@
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
-import React, { Component, StrictMode } from 'react'
+import React, { Component } from 'react'
 import '../../../css/maps.css';
-import 'mapbox-gl/dist/mapbox-gl.css';
-import  'leaflet/dist/leaflet.css';
 import 'leaflet/dist/leaflet'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
 
+
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 mapboxgl.accessToken = 'pk.eyJ1IjoicmFua3JoIiwiYSI6ImNraDFnbjlrcTAxZjMydG4xN2dyNmtoYWUifQ.tlJBm2GyxVZapHdK0_oDyQ';
 
 export class Map extends Component {
-    constructor(props) {
-        super(props);
 
-        this.state = {
-            lng: props.lng ? props.lng : -100,
-            lat: props.lat ? props.lat : 40,
-            zoom: props.zoom ? props.zoom : 3,
-            size: props.size ? "map-" + props.size : "map-2"
-        };
-
-        this.mapContainer = React.createRef();
-    }
-
-    componentDidMount() {
-
-        const map = new mapboxgl.Map({
-            container: this.mapContainer.current,
-            style: 'mapbox://styles/rankrh/ckhcwof2w17fa19o2uo8pmzdr',
-            center: [this.state.lng, this.state.lat],
-            zoom: this.state.zoom
-        });
-        
-        map.on('move', () => {
-            this.setState({
-                lng: map.getCenter().lng.toFixed(4),
-                lat: map.getCenter().lat.toFixed(4),
-                zoom: map.getZoom().toFixed(2)
-            });
-        });
-    }
-
-
-
+    
     render() {
         return(
-            <div ref={ this.mapContainer } className={ this.state.size }/>
+            <MapContainer className="map-2" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://api.mapbox.com/styles/v1/rankrh/ckhcwof2w17fa19o2uo8pmzdr/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoicmFua3JoIiwiYSI6ImNraDFnbjlrcTAxZjMydG4xN2dyNmtoYWUifQ.tlJBm2GyxVZapHdK0_oDyQ"
+                />
+                <Marker position={[51.505, -0.09]}>
+                    <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
         );
     }
 }
